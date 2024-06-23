a9e6d4fbc4dcb548de2f115154a4d5c3fc47023c
krecik71 <37263381+krecik71@users.noreply.github.com>
2024-06-23 20:30:09 +0200

    Add files via upload

---
+++ Programmlisten Updater.asd.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Programmlisten Updater</title>
+  </head>
+  <body>
+    <table cellspacing="0" cellpadding="0" border="0">
+      <tbody>
+        <tr>
+          <td><a href="http://www.sattechnik.de/programmlisten-update/Enigma2-D-19E-FTA.zip">Enigma2 D 19E FTA</a></td>
+          <td>240609</td>
+        </tr>
+        <tr>
+          <td><a href="http://www.sattechnik.de/programmlisten-update/Enigma2-D-19E-PayTV.zip">Enigma2 D 19E PayTV</a></td>
+          <td>240609</td>
+        </tr>
+        <tr>
+          <td><a href="http://www.sattechnik.de/programmlisten-update/Enigma2-EU-19E-PayTV.zip">Enigma2 EU 19E PayTV</a></td>
+          <td>240609</td>
+        </tr>
+        <tr>
+          <td><a href="http://www.sattechnik.de/programmlisten-update/Enigma2-EU-19+13E-PayTV.zip">Enigma2 EU 19+13E PayTV</a></td>
+          <td>240609</td>
+        </tr>
+        <tr>
+          <td><a href="http://www.sattechnik.de/programmlisten-update/Enigma2-EU-19+13E-PayTV-UHD.zip">Enigma2 EU 19+13E PayTV UHD</a></td>
+          <td>240609</td>
+        </tr>
+        <tr>
+          <td><a href="http://www.sattechnik.de/programmlisten-update/Enigma2-EU-19+13+23E-PayTV.zip">Enigma2 EU 19+13+23.5E PayTV</a></td>
+          <td>240609</td>
+        </tr>
+      </tbody>
+    </table>
+ 
+  
+
+</body></html>
